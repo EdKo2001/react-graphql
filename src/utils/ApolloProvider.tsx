1c16f9eb85ba36efa62e014f408d923451c36bc4
Eduard Koshkelyan <eduardko2001@gmail.com>
2023-11-02 21:10:35 +0400

    feat(graphql-requests): add

--- src/utils/ApolloProvider.tsx
+++ src/utils/ApolloProvider.tsx
@@ -3,16 +3,33 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider as ApolloClientProvider,
+  ApolloLink,
+  HttpLink,
 } from "@apollo/client";
 
 import env from "./env";
+import { getAccessToken, isLoggedIn } from "./auth";
 
 interface ApolloProviderProps {
   children: ReactNode;
 }
 
-const client = new ApolloClient({
-  uri: env.REACT_APP_APOLLO_URL,
+const authLink = new ApolloLink((operation, forward) => {
+  if (isLoggedIn()) {
+    operation.setContext({
+      headers: {
+        authorization: "Bearer " + getAccessToken(),
+      },
+    });
+  }
+  return forward(operation);
+});
+
+export const client = new ApolloClient({
+  link: ApolloLink.from([
+    authLink,
+    new HttpLink({ uri: env.REACT_APP_APOLLO_URL }),
+  ]),
   cache: new InMemoryCache(),
 });
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
 export { default as env } from "./env";
 export { getAccessToken, login, isLoggedIn, logout } from "./auth";
-export { default as ApolloProvider } from "./ApolloProvider";
+export { default as ApolloProvider, client } from "./ApolloProvider";
